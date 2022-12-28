89584e0b858e810a6c0c88f230269d0e92b0fbc3
Delavalom <laarvelo@icloud.com>
2022-12-28 19:48:23 -0400

    add formidable to the project

--- src/pages/api/cloudinary.ts
+++ src/pages/api/cloudinary.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { UploadApiOptions, v2 as cloudinary } from "cloudinary";
+import { parseForm, FormidableError } from "utils/parse-form";
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_ID,
@@ -13,15 +14,31 @@ export default async function handler(
   response: NextApiResponse
 ) {
   const METHOD = request.method;
-  if (METHOD === "POST") {
-    try {
-      return await uploadImage(request, response);
-    } catch (error) {
-      if (error instanceof Error) {
-        console.log(error.message)
-      };
-      console.log(error)
+
+  if (METHOD !== "POST") {
+    response.setHeader("Allow", "POST");
+    response.status(405).json({
+      data: null,
+      error: "Method not allowed",
+    });
+    return;
+  }
+
+  try {
+    const {fields, files} = await parseForm(request)
+    console.log({fields, files})
+    response.status(200).json({
+      data: {
+        url: '',
+      },
+      error: null   
+    })
+  } catch (error) {
+    if (error instanceof FormidableError) {
+      response.status(error.httpCode || 400).json({ data: null, error: error.message})
     }
+    console.log(error);
+    response.status(500).json({ data: null, error: 'Internal Server Error'})
   }
 }
 
@@ -29,7 +46,7 @@ const uploadImage = async (
   request: NextApiRequest,
   response: NextApiResponse
 ) => {
-  const { file } = request.body;
+  const file = request.body;
 
   const options: UploadApiOptions = {
     public_id: "build",
@@ -43,8 +60,12 @@ const uploadImage = async (
       .status(201)
       .json({ public_id: result.public_id, url: result.secure_url });
   } catch (error) {
-    return response
-      .status(400)
-      .json({ message: "Couldn't upload file" });
+    return response.status(400).json({ message: "Couldn't upload file" });
   }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
 interface Props {}
 
@@ -8,26 +8,36 @@ const Upload = () => {
   const [filePreview, setFilePreview] = useState<string>("");
   const [message, setMessage] = useState("Upload build image");
 
-  const handleFile = ({
+  const onFileUploadChange = ({
     target,
-  }: React.ChangeEvent<HTMLInputElement>): void => {
+  }: ChangeEvent<HTMLInputElement>): void => {
     const { files } = target;
-    if (!files) throw new Error("no funciono");
-
+    if (!files || files.length === 0) throw alert("No file was chosen");
+    if (!files[0].type.startsWith("image")) throw alert('please select a valide Image')
+    
     setFilePreview(URL.createObjectURL(files[0]));
     setFile(files[0]);
     setMessage("Upload build image");
-  };
 
-  const uploadFile = async () => {
+    if(!file) return
+    
+    target.type = 'text';
+    target.type = 'file';
+  };
+  
+  const onUploadFile = async () => {
+    if(!file) return
+
+    const formData = new FormData()
+    formData.append('media', file)
+
     setMessage("loading...");
-    // const response = await fetch('/api/cloudinary', {
-    //   method: 'POST',
-    //   headers: {'Content-Type': 'application/json'},
-    //   body: JSON.stringify({ file: file?.name})
-    // })
-    // const data = await response.json()
-    // console.log(data)
+    const response = await fetch('/api/cloudinary', {
+      method: 'POST',
+      body: formData
+    })
+    const data = await response.json()
+    console.log(data)
 
     setMessage("Not yet available");
   };
@@ -36,7 +46,7 @@ const Upload = () => {
     <section className="h-1/2 flex flex-col justify-between items-center">
       <input
         accept="image/*"
-        onChange={handleFile}
+        onChange={onFileUploadChange}
         type="file"
         className="block w-full text-sm text-slate-500
       file:mr-4 file:py-2 file:px-4
@@ -56,7 +66,7 @@ const Upload = () => {
             className="rounded-sm"
           />
           <button
-            onClick={uploadFile}
+            onClick={onUploadFile}
             className="text-slate-700 px-20 py-2 rounded-full bg-slate-50"
           >
             {message}
