54bf43d702e51cfb1a22ce25bd3ad117327db61c
Delavalom <laarvelo@icloud.com>
2023-05-09 10:08:01 -0400

    ui library update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,16 @@
-export const dateFormatter = (date: Date | undefined) => {
-    if (!date) return ``
-    const minutes = date.getMinutes()
-    const day = date.getDay()
-    const month = date.toLocaleString('en-us', { month: "long" })
-    const year = date.getFullYear()
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-    return `${month}, ${day} ${year}` as const
+export const dateFormatter = (date: Date | undefined) => {
+  if (!date) return ``;
+  const minutes = date.getMinutes();
+  const day = date.getDay();
+  const month = date.toLocaleString("en-us", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${month}, ${day} ${year}` as const;
+};
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
