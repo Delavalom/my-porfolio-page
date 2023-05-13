9466e353339bda1e0ae9db46ab680457512797cc
Delavalom <laarvelo@icloud.com>
2023-05-12 22:01:10 -0400

    tailwind dark utility

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -12,7 +12,7 @@ export const HeaderBar: FC<Props> = ({ title, Cta }) => {
   const { isOpen, setIsOpen } = useNavigation();
 
   return (
-    <div className="w-full max-w-4xl h-14 p-4 flex items-center gap-4 sticky top-0 backdrop-blur-2xl">
+    <div className="w-full max-w-4xl h-14 p-4 flex items-center gap-4 sticky top-0 backdrop-blur-2xl dark:text-slate-200">
       {!isOpen ? (
         <Button className="w-fit" variant="ghost">
           <Menu onClick={() => setIsOpen(true)} />
