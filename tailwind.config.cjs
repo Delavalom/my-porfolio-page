7cfba3cd2b77e2ec5865229889e47a2683cf38ae
Delavalom <laarvelo@icloud.com>
2023-05-13 11:52:50 -0400

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
+  darkMode: ["class"],
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./src/pages/**/*.{js,ts,jsx,tsx}",
